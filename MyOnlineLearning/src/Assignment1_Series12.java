e0c5e4126c2110893933d58c83b03c329fda1f10
bojjkir <kiran.bojja@vw.com>
2021-01-03 16:41:09 -0500

    Uploading Assignment1

---
+++ MyOnlineLearning/src/Assignment1_Series12.java
@@ -0,0 +1,19 @@
+
+public class Assignment1_Series12
+{
+
+	public static void main(String[] args) 
+	{
+		int size = 5;
+		for (int i=1;i<=size;i++)
+		{
+			for (int j=1;j<=i;j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
