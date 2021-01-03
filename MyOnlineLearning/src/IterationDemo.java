6c13d0ea961e81588eaff591fbba8132044366d4
bojjkir <kiran.bojja@vw.com>
2021-01-03 15:51:02 -0500

    First Commit
    Uploading all files

---
+++ MyOnlineLearning/src/IterationDemo.java
@@ -0,0 +1,18 @@
+
+public class IterationDemo 
+{
+
+	public static void main(String[] args) 
+	{
+		for(int i=1;i<=4;i++)
+		{
+			for(int j=1; j<=4;j++)
+			{
+			System.out.print(j+"");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
