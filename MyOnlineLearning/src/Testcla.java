988dbe6bd3822ee2ed8d6f6dda55850dbe9c003b
kbojja21 <76918102+kbojja21@users.noreply.github.com>
2021-01-06 18:21:34 -0500

    Create Testcla.java

---
+++ MyOnlineLearning/src/Testcla.java
@@ -0,0 +1,28 @@
+class CheckMerge
+{
+	public void merge()
+	{
+		System.out.println("in merge from local");
+	}
+	
+	public void merge2()
+	{
+		
+	}
+}
+
+public class TestCla 
+{
+
+	public static void main(String[] args) 
+	{
+		// Test class to check the merge issue
+		
+		CheckMerge chc = new CheckMerge();
+		chc.merge();
+		
+		
+
+	}
+
+}
