b241307569295d0e6e5b043c20c9e7b05cfc2a02
KIRAN KUMAR <kirankumar.marketing21@gmail.com>
2021-01-06 17:18:49 -0500

    Merge testing

---
+++ MyOnlineLearning/src/TestCla.java
@@ -0,0 +1,20 @@
+class CheckMerge
+{
+	public void merge()
+	{
+		
+	}
+}
+
+public class TestCla 
+{
+
+	public static void main(String[] args) 
+	{
+		// Test class to check the merge issue
+		
+		
+
+	}
+
+}
