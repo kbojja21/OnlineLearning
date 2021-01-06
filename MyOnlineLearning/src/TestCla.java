b35de35166a6aebf870730eed85e37ad50b33bae
KIRAN KUMAR <kirankumar.marketing21@gmail.com>
2021-01-06 17:23:46 -0500

    Added syso in merge method

--- MyOnlineLearning/src/TestCla.java
+++ MyOnlineLearning/src/TestCla.java
@@ -2,7 +2,7 @@ class CheckMerge
 {
 	public void merge()
 	{
-		
+		System.out.println("in merge from local");
 	}
 }
 
@@ -12,6 +12,7 @@ public class TestCla
 	public static void main(String[] args) 
 	{
 		// Test class to check the merge issue
+		
 		
 		
 
